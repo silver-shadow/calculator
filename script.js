70093e1b8ee218a5c2efe4ee333875f04551d3a8
silver-shadow <107355428+silver-shadow@users.noreply.github.com>
2023-06-22 17:36:03 +0530

    Set up display functionality

--- script.js
+++ script.js
@@ -38,3 +38,27 @@ function operate(op,n1,n2)
         divide(Number(n1),Number(n2));
     }
 }
+function dispNum(num)
+{
+    const display = document.querySelector(".display");
+    if(!isNaN(num))
+    {
+        display.textContent = display.textContent + num;
+    }
+    return display;
+}
+function pressKey()
+{
+    const screenKeys = document.querySelectorAll("button");
+    screenKeys.forEach(key => {
+        key.addEventListener("mousedown",() =>{
+            dispNum(key.textContent);
+        })
+    });
+    document.addEventListener('keydown', (e) => {
+        //console.log('Key pressed:', e.key, typeof(e.key));
+        dispNum(e.key);
+      });
+}
+
+pressKey()
